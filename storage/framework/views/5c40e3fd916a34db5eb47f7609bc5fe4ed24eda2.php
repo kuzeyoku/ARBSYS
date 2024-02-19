4391c6768439356b5701fdc95f9dfb2b59baa327
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-02-19 14:26:08 +0300

    clear

--- storage/framework/views/5c40e3fd916a34db5eb47f7609bc5fe4ed24eda2.php
+++
@@ -1,7 +0,0 @@
-<h1 class="title">ARABULUCU</h1>
-<p>
-    Arb. Av. <?php echo e(App\Services\HelperService::nameFormat(auth()->user()->name)); ?> <br />
-    (ADB Sicil No: <?php echo e(auth()->user()->mediator->registration_no); ?>)<br />
-    
-</p>
-<?php /**PATH C:\laragon\www\arbsys\resources\views/mediator/document/mediator_info.blade.php ENDPATH**/ ?>
