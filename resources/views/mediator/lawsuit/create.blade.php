deee51b281ea8cb2b74fd8d5668ea3c053793c2b
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-12-12 22:30:09 +0300

    Revert "11122024"
    This reverts commit de6ecf2bee86287c15a4c8e4b6cb5704d82d3f20.

--- resources/views/mediator/lawsuit/create.blade.php
+++ resources/views/mediator/lawsuit/create.blade.php
@@ -369,13 +369,9 @@
                                     <span></span>
                                 </label>
                                 <label class="kt-radio">
-                                    <input type="radio" data-type="company_private" name="isPerson" class="personTypeSelect">
-                                    Tüzel Kişi (Özel Hukuk)
-                                    <span></span>
-                                </label>
-                                <label class="kt-radio">
                                     <input type="radio" data-type="company_public" name="isPerson" class="personTypeSelect">
-                                    Tüzel Kişi (Kamu Hukuku)
+                                    Tüzel
+                                    Kişi
                                     <span></span>
                                 </label>
                             </div>
